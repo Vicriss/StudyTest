dc79ced3cb524fc2d728884c3c5b3faf0b633589
vicriss <412646974@qq.com>
2016-02-13 13:04:37 +0800

    using logger and thread pool

--- src/thread/racecondition/EnergySystemTest.java
+++ src/thread/racecondition/EnergySystemTest.java
@@ -1,6 +1,10 @@
 package thread.racecondition;
 
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class EnergySystemTest {
 
 	//将要构建的能量世界中能量盒子数量
@@ -10,10 +14,12 @@ public class EnergySystemTest {
 
     public static void main(String[] args){
     	EnergySystem eng = new EnergySystem(BOX_AMOUNT, INITIAL_ENERGY);
-    	for (int i = 0; i < BOX_AMOUNT; i++){
-    		EnergyTransferTask task = new EnergyTransferTask(eng, i, INITIAL_ENERGY);
-    		Thread t = new Thread(task,"TransferThread_"+i);
-    		t.start();
+		ExecutorService es = Executors.newCachedThreadPool();
+		for (int i = 0; i < BOX_AMOUNT; i++){
+			EnergyTransferTask task = new EnergyTransferTask(eng, i, INITIAL_ENERGY);
+			es.execute(task);
+//    		Thread t = new Thread(task,"TransferThread_"+i);
+//    		t.start();
     	}
     }
 
